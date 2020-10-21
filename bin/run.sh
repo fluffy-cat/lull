5010d0875b2943b6723b81cd65c7908c1aedf5d0
Daniel <dan@dan.com>
2020-10-21 15:07:08 +0000

    Fixed wrong module in start script

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,4 @@
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
-python -m plexpost /app/default_config.yml /config/config.yml
+python -m lull /app/default_config.yml /config/config.yml
