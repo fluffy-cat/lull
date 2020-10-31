a04aabbd1b88abff0ec8e64b8eacf3e5a71b4039
Daniel <dan@dan.com>
2020-10-31 09:30:25 +0000

    Fix test fails from previous commit

--- tests/test_tcp_monitor.py
+++ tests/test_tcp_monitor.py
@@ -5,7 +5,7 @@ import pytest
 
 from lull.tcp_monitor import TCPMonitor
 
-Conn = namedtuple('Connection', ['laddr', 'status'])
+Conn = namedtuple('Connection', ['laddr', 'raddr', 'status'])
 Addr = namedtuple('Address', ['ip', 'port'])
 
 
@@ -21,29 +21,31 @@ def test_shouldNotReturnKeepAlive_whenThereAreNoConnections(psutil):
 
 def test_shouldReturnKeepAlive_whenThereIsAnActiveConnection(psutil):
     monitor = TCPMonitor([22], psutil)
-    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 22), 'ESTABLISHED')], 1, monitor, psutil)
+    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 22), Addr('', 123), 'ESTABLISHED')], 1, monitor,
+                                       psutil)
 
 
 def test_shouldNotReturnKeepAlive_whenConnectionIsInactive(psutil):
     monitor = TCPMonitor([22], psutil)
     assert_keepalive_given_connections(
-        [Conn(Addr('192.168.245.130', 22), 'CLOSE'), Conn(Addr('192.168.245.130', 22), 'CLOSE_WAIT')], 0, monitor,
-        psutil)
+        [Conn(Addr('192.168.245.130', 22), None, 'CLOSE'), Conn(Addr('192.168.245.130', 22), None, 'CLOSE_WAIT')], 0,
+        monitor, psutil)
 
 
 def test_shouldNotReturnKeepAlive_whenConnectionIsFromAnotherPort(psutil):
     monitor = TCPMonitor([22], psutil)
 
-    assert_keepalive_given_connections(
-        [Conn(Addr('192.168.245.130', 80), 'ESTABLISHED'), Conn(Addr('192.168.245.130', 443), 'ESTABLISHED')], 0,
-        monitor, psutil)
+    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 80), None, 'ESTABLISHED'),
+                                        Conn(Addr('192.168.245.130', 443), None, 'ESTABLISHED')], 0, monitor, psutil)
 
 
 def test_shouldReturnKeepAlive_whenMultiplePortsAreMonitored(psutil):
     monitor = TCPMonitor([22, 23], psutil)
-    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 22), 'ESTABLISHED')], 1, monitor, psutil)
-    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 23), 'ESTABLISHED')], 1, monitor, psutil)
-    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 24), 'ESTABLISHED')], 0, monitor, psutil)
+    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 22), Addr('', 123), 'ESTABLISHED')], 1, monitor,
+                                       psutil)
+    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 23), Addr('', 123), 'ESTABLISHED')], 1, monitor,
+                                       psutil)
+    assert_keepalive_given_connections([Conn(Addr('192.168.245.130', 24), None, 'ESTABLISHED')], 0, monitor, psutil)
 
 
 def assert_keepalive_given_connections(connections, keepalive, monitor, psutil):
